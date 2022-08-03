1c8c894067d08155c91a272c775c328b1c380b82
Roshan <roshanpanta2059@gmail.com>
2022-08-03 09:55:54 +0545

    completed course upto day 2

---
+++ day1-c.py
@@ -0,0 +1,21 @@
+a = 90
+b = 40
+c = "hello"
+d = True
+e = a + b
+
+print(a, b, c, d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(a + b)
+print(e)
+print(a - b)
+
+# ** power
+# // floor division only prints the whole number
+
+print(a // b)
+print(a % b)
---
+++ day2-c.py
@@ -0,0 +1,37 @@
+# a = "hello "
+# b = "world\n"
+# c = a + b
+# print(c)
+
+# print(c*2)
+
+# a = True
+# b = False
+# print(a+b)
+
+# a = int(input("Enter the value of a:"))
+# b = int(input("Enter the value of c:"))
+# c = a - b
+# print(c)
+
+# p = int(input("Enter the principal:"))
+# t = float(input("Enter the time:"))
+# r = float(input("Enter the rate:"))
+
+# i = (p*t*r)/100
+
+# print(int(i))
+
+# r = float(input("Enter the value of radius:"))
+# area = 3.14*r**2
+# print("The value of area is:",area)
+
+a = 10
+print(float(a))
+print(str(a))
+
+name = input("Enter your name:")
+age = int(input("Enter age:"))
+address = input("Enter your address:")
+info = "My name is " + name + " I am from " + address + " I am " + str(age) + " years old"
+print(info)
