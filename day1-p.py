e418254955d8c33671a285cc162b6a8f1eedf3e3
Roshan <roshanpanta2059@gmail.com>
2022-08-04 10:08:13 +0545

    day1 home practice done

---
+++ day1-p.py
@@ -0,0 +1,17 @@
+a = 17
+b = 5
+c = 6.7
+d = "hello"
+e = "world"
+f = True
+
+print(a + c)
+print(d + e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(a**2)
+print(a//b)
