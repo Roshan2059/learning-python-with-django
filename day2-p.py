68ed8119a93ffa49880bae0c40a09862df49f65b
Roshan <roshanpanta2059@gmail.com>
2022-08-04 11:03:48 +0545

    day2 home practice done

---
+++ day2-p.py
@@ -0,0 +1,16 @@
+# a = int(input("Enter a number:\n"))
+# print("Its square is: ",a**2)
+
+# b = int(input("Enter first number:\n"))
+# c = int(input("Enter second number:\n"))
+# print("The added result is : ", b+c)
+
+# d = input("Enter first number:\n")
+# e = input("Enter second number:\n")
+# print("The concatenated result is: ",d+e)
+
+a = 20
+print(float(a))
+print(str(a))
+
+
