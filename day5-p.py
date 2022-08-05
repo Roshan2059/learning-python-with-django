300afce985c0e774df4e5b29166afb632a0718c7
Roshan <roshanpanta2059@gmail.com>
2022-08-05 22:56:05 +0545

    half done of day5 practice

---
+++ day5-p.py
@@ -0,0 +1,32 @@
+# a = "roshan"
+# for i in a:
+#     print(i)
+    
+# a = "hello"
+# for i in a:
+#     print(i," world")
+
+# for i in a:
+#     print(i, end = "web")
+    
+a = "hello world"
+for i in a:
+    if i != " ":
+        print(i)
+        
+b = "hi roshan"
+for i in b:
+    if i != "a":
+        break
+    print(i)
+    
+    c = "hi roshan"
+for i in b:
+    if i != "a":
+        break
+    print(i)
+    
+    for i in b:
+    if i != "a":
+        
+    continue(i)
