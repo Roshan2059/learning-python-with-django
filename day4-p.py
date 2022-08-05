c42d53fd9d1703d05e766cddbe726bf05e69b60b
Roshan <roshanpanta2059@gmail.com>
2022-08-05 21:13:56 +0545

    day5 practice left

--- day4-p.py
+++ day4-p.py
@@ -3,11 +3,30 @@
 # Total = price * quantity.
 # The output should be in string format.
 
+# indtotal = 0
+# total = 0
+# n = int(input("How many different products did you buy? :"))
+# for i in range(1,n+1):
+#  name = input("Enter the product's name: \n")
+#  price = int(input("Enter the price: \n"))
+#  quantity = int(input("Enter the quantity\n"))
+#  indtotal = (price * quantity)
+#  print("You bought " + str(quantity) + " " + name + " which is Rs." + str(indtotal))
+#  total = total + indtotal
+ 
+# print("Please pay Rs."+str(total)+" in the counter")
+
+# In the normal bill format
+indtotal = 0
 total = 0
-n = int(input("How many different products did you buy? :"))
+n = int(input("How many different products did you buy? :\n"))
 for i in range(1,n+1):
- name = input("Enter the product's name: \n")
- price = int(input("Enter the price: \n"))
- quantity = int(input("Enter the quantity\n"))
- total = total + (price * quantity)
-print("Please pay Rs."+str(total)+" in the counter")
+    name = input("Enter the product's name: \n")
+    price = int(input("Enter the price: \n"))
+    quantity = int(input("Enter the quantity\n"))
+    indtotal = indtotal + (price * quantity)
+    total = total + indtotal
+print("particulars" + "  " + "quantity" + "  " + "rate" + "  total") 
+for i in range(1, n+1):
+    print(name + "          " + str(quantity) +"       "+ str(price) +"     "+ str(indtotal) + "\n" )
+print("Grand total:" + "                 " + str(total))
---
+++ day5-c.py
@@ -0,0 +1,66 @@
+total = 0
+n = int(input("How many different products did you buy? :"))
+for i in range(1, n + 1):
+    name = input("Enter the product's name: \n")
+    price = int(input("Enter the price: \n"))
+    quantity = int(input("Enter the quantity\n"))
+    total = total + (price * quantity)
+    bill = name + " " + str(price) + " " + str(quantity)
+print("particulars" + " " + "price" + " " + "quantity" + " " + "total")
+print("Please pay Rs." + str(total) + " in the counter")
+
+a = "python"
+for i in a:
+  print(i)
+# string is a sequence of characters
+
+a = "python"
+for i in a:
+  print(i, "hello")
+
+a = "hello  world\n"
+for i in a:
+  if i != " ":
+    print(i, end = "")
+
+# end is just a keyword in python
+a = "hello  world\n"
+print("The letters are:")
+for i in a:
+  if i != " ":
+    print(i, end = ",")
+
+a = "hello  world."
+for i in a:
+  if i != " " and i !=".":
+    print(i, end = "")
+
+# control statement:
+# break
+# continue
+# pass
+
+# break statement:
+for i in range(1,10):
+  if i == 5:
+    break
+  print(i)
+
+a = "hello  world."
+for i in a:
+    if i == " ":
+        break
+    print(i, end="")
+
+# continue statement:
+for i in range(1,5):
+    if i == 3:
+      continue
+    print(i)
+
+a = "hello  world\n"
+print("The letters are:")
+for i in a:
+  if i == " ":
+    continue
+  print(i, end = "")
