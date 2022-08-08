83a5f598414d2e5e180d42b92a837a5c0272076e
Roshan <roshanpanta2059@gmail.com>
2022-08-08 14:30:45 +0545

    completely updated upto day6

---
+++ day6-p.py
@@ -0,0 +1,11 @@
+# n = int(input("Enter a number"))
+# i = 1
+# while i < 11:
+#     print(n , "*" , i , "=" , n*i)
+#     i = i +1
+    
+a = "Hi! I am Roshan"
+i = 0
+while i < len(a):
+    print(a[i],end = "")
+    i += 1
