ac60c017dde90cdcacbb563fd3bdcfd8864e66bd
Roshan <roshanpanta2059@gmail.com>
2022-08-09 21:48:03 +0545

    initiated day8 home practice

---
+++ day8-p.py
@@ -0,0 +1,11 @@
+def add():
+  a = int(input("enter the first number:\n"))
+  b = int(input("Enter the second number:\n"))
+  result = a + b
+  print(result)
+  o = input("Do you want to perform the operation again? If yes press y else press any key:\n")
+  if o == "y" | o == "Y":
+    print("Result: ",add())
+  else:
+      print("Operation terminated")
+add()
