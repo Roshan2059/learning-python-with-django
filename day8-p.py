eaee2d736724145b8078ab58d87f4c7f7dbc9f63
Roshan <roshanpanta2059@gmail.com>
2022-08-09 22:25:17 +0545

    day8 home practice left

--- day8-p.py
+++ day8-p.py
@@ -2,10 +2,11 @@ def add():
   a = int(input("enter the first number:\n"))
   b = int(input("Enter the second number:\n"))
   result = a + b
-  print(result)
+  print("The result is: ",result)
   o = input("Do you want to perform the operation again? If yes press y else press any key:\n")
-  if (o) == "y" | o == "Y":
+  if o == "y" or o == "Y":
     print("Result: ",add())
   else:
       print("Operation terminated")
+
 add()
