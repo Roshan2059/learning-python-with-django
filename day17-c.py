d9b9ca62affbc0dfe3c46bd497dcde019fc0e1ee
Roshan Panta <roshanpanta2059@gmail.com>
2022-08-23 22:54:37 +0545

    In love with python,csv and pandas

--- day17-c.py
+++ day17-c.py
@@ -1,50 +1,65 @@
-# day17
+# # day17
 
-try:
-  b = open('data.txt','x')
-  b.close()
-except:
-  print('File already exists')
+# try:
+#   b = open('data.txt','x')
+#   b.close()
+# except:
+#   print('File already exists')
 
-b =  open('data.txt','r')
-x = b.read()
-print(x)
-b.close()
+# b =  open('data.txt','r')
+# x = b.read()
+# print(x)
+# b.close()
 
-y = x.split('\n')
-print(y)
+# y = x.split('\n')
+# print(y)
 
-a = open('data1.txt','x')
-a.close()
+# a = open('data1.txt','x')
+# a.close()
 
-with open('data1.txt','w') as b:
-  b.write('I am Roshan')
+# with open('data1.txt','w') as b:
+#   b.write('I am Roshan')
 
-# It is same as b = open('data1.txt','w') but if we write the with syntax then we should not have t0 write close function
+# # It is same as b = open('data1.txt','w') but if we write the with syntax then we should not have t0 write close function
 
-b = open('data2.txt','w')
-b.write('Hello world! Its Roshan')
-b.close()
+# b = open('data2.txt','w')
+# b.write('Hello world! Its Roshan')
+# b.close()
+
+# bill = ''
+# n = int(input('Enter n = '))
+# for i in range(n):
+#   name = input('Enter name = ')
+#   price = int(input('Enter price = '))
+#   quantity = int(input('Enter quantity = '))
+#   total = price * quantity
+#   data = f"{name} {price} {quantity} {total}\n"
+#   bill = bill+data
+# print(bill)
+
+# b = open('bill.txt','w')
+# b.write(bill)
+# b.close()
+
+# bill = ''
+# n = int(input('Enter n = '))
+# for i in range(n):
+#   name = input('Enter name = ')
+#   price = int(input('Enter price = '))
+#   quantity = int(input('Enter quantity = '))
+#   total = price * quantity
+#   data = f"{name},{price},{quantity},{total}\n"
+#   bill = bill+data
+# print(bill)
+
+# b = open('bill.csv','w')
+# b.write(bill)
+# b.close()
 
 bill = ''
 n = int(input('Enter n = '))
 for i in range(n):
-  name = input('Enter name = ')
-  price = int(input('Enter price = '))
-  quantity = int(input('Enter quantity = '))
-  total = price * quantity
-  data = f"{name} {price} {quantity} {total}\n"
-  bill = bill+data
-print(bill)
-
-b = open('bill.txt','w')
-b.write(bill)
-b.close()
-
-bill = ''
-n = int(input('Enter n = '))
-for i in range(n):
-  name = input('Enter name = ')
+  name = input('Enter product name = ')
   price = int(input('Enter price = '))
   quantity = int(input('Enter quantity = '))
   total = price * quantity
@@ -53,5 +68,10 @@ for i in range(n):
 print(bill)
 
 b = open('bill.csv','w')
+b.write('Particulars,price,quantity,total\n')
 b.write(bill)
 b.close()
+
+import pandas as p
+a = p.read_csv('bill.csv')
+print(a)
